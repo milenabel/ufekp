60c4f80b614ceba8041378306700aa7f85cdc5f6
Milena Belianovich <u1419504@umail.utah.edu>
2025-07-11 16:40:31 -0600

    final code and plot commits

--- code/Approximation/CleanLagrangeApprox/TorusTest.m
+++ code/Approximation/CleanLagrangeApprox/TorusTest.m
@@ -40,8 +40,27 @@ for k=1:length(n)
     f_x(isnan(f_x)) = 0;
 
     %% Get the polynomial degree. We'll use ambient polynomials in R^3
-    ell = floor(0.8*nthroot(length(x),3));   
+    ell = floor(nthroot(length(x),3));   
+
+    %% Do poly least squares with rank-revealing QR
+    [el2_poly(k,1),elinf_poly(k),a_time_poly(k),e_time_poly(k),~] = PLSManifold(x,f_x,ell,xe,0,f_xe);
+    
 
     %% Do a CSRBF + poly fit that uses a rank-revealing QR
-   [el2(k,1),elinf(k,1),a_time(k,1),e_time(k,1),~,cond_num(k,1),~, sparsity(k,1)] = CSRBFGenManifold(x,f_x,ell,xe,0,rbf,5,tree,f_xe);   
+   [el2(k,1),elinf(k,1),a_time(k,1),e_time(k,1),~,cond_num(k,1),~, sparsity(k,1)] = CSRBFGenManifold(x,f_x,ell,xe,0,rbf,7,tree,f_xe);   
 end
+
+h=figure;
+set(h, 'Color', 'none');          
+ax = gca;
+set(ax, 'Color', 'none','FontSize', 12,'LineWidth', 1.2,'TickLabelInterpreter', 'latex');     
+marks = {'-o','-s','-^','-x','-+'};
+semilogy(sqrt(n), el2(:),marks{1}, 'LineWidth',1.2);
+hold on;
+semilogy(sqrt(n), el2_poly(:),marks{2}, 'LineWidth',1.2);
+xlabel('N^{1/2}','Interpreter','tex','FontSize',14,'FontWeight','bold');
+ylabel('Relative l_2 error','Interpreter','tex','FontSize',14,'FontWeight','bold');
+legend({'FS','PLS'}, 'Location','best','Interpreter','tex','FontWeight','bold','FontSize',14);
+title(sprintf('Relative l_2 error vs. N^{1/d}, C^2(R^3) Wendland Kernel'),'Interpreter','tex','FontWeight','bold','FontSize',14);
+%export_fig(gcf, fullfile(res.results_dir,'error_vs_N_fs_s%d.png'),'-png','-r300','-transparent');
+%close(h);
